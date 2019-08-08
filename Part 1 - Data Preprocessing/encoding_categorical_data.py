6df2e6c1a9129cfb8d3faf77526cecb60e418cdc
Aousnik Gupta <43369060+gittygupta@users.noreply.github.com>
2019-08-08 20:40:08 +0530

    Update encoding_categorical_data.py

--- Part 1 - Data Preprocessing/encoding_categorical_data.py
+++ Part 1 - Data Preprocessing/encoding_categorical_data.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from sklearn.preprocessing import LabelEncoder
 labelencoder_country = LabelEncoder()
 x[:, 0] = labelencoder_country.fit_transform(x[:, 0])
@@ -14,4 +8,3 @@ data = pd.DataFrame({'Country':x[:,0],'Age':x[:,1], 'Salary':x[:,2]})
 data = data.drop('Country', axis = 1)
 data = pd.concat([m, data], axis=1)
 x = data.iloc[:, :].values
-
