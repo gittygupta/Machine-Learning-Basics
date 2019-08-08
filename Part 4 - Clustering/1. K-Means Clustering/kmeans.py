e1da51f9e383e21da7543a5fe08a7fa34da4b63d
Aousnik Gupta <43369060+gittygupta@users.noreply.github.com>
2019-08-08 21:13:01 +0530

    Update kmeans.py

--- Part 4 - Clustering/1. K-Means Clustering/kmeans.py
+++ Part 4 - Clustering/1. K-Means Clustering/kmeans.py
@@ -1,20 +1,5 @@
 # K-Means Clustering
 
-'''
-What actually happens here is we don't have any dependent variable as we don't know
-how the clustering of the data is done. So we take only the last 2 independent variables i.e,
-Salary and spending score. And based on this we cluster our data
-y_kmeans is the dependent variable containing the clustered data
-We can classify the data as - 1. Spends with low salary
-                              2. Careful with spendings
-                              etc, etc...
-As in classifier we had the y variable and 2 independent variables which predicted the 2 classes
-In this case we feed the kmeans object the 2 dependent variables which then clusters the data
-to give the y_kmeans dependent variable
-
-This is actually what happens in the program. 
-'''
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -27,10 +12,6 @@ dataset = pd.read_csv('Mall_Customers.csv')
 x = dataset.iloc[:, [3, 4]].values
 
 # Using the elbow method and graph to find the optimal number of clusters
-'''
-We use a loop to find wcss for clusters 1 to 10
-Another name for wcss is 'inertia' and sklearn has a function that finds out wcss for us
-'''
 from sklearn.cluster import KMeans
 wcss = []
 for i in range(1, 11):
@@ -51,10 +32,6 @@ In this case we have number of optimal clusters as 5
 '''
 
 # Now we fit the dataset with the optimal number of clusters
-'''
-fit_predict method returns the clustered data i.e, for each datapoint, 
-it returns the value of each cluster it belongs to
-'''
 kmeans = KMeans(n_clusters = 5, init = 'k-means++', random_state = 0)
 y_kmeans = kmeans.fit_predict(x)
 
@@ -96,17 +73,3 @@ plt.xlabel('Annual Income (k$)')
 plt.ylabel('Spending Score')
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
